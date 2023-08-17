8fe5e1aceda859a05980ba1c9c53f439fe7b7181
Andrew Warner <warneracw@gmail.com>
2023-08-17 11:55:11 -0400

    change license to MIT

--- contracts/Kettle.sol
+++ contracts/Kettle.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "solmate/src/tokens/ERC20.sol";
-
 import "./Helpers.sol";
 import "./CollateralVerifier.sol";
 
--- contracts/escrow/ERC1155EscrowBase.sol
+++ contracts/escrow/ERC1155EscrowBase.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Skillet
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
