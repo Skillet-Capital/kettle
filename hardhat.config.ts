82782c41c33219f3f2337f151abc11592163a316
Andrew Warner <warneracw@gmail.com>
2023-09-07 12:23:17 -0400

    run optimizer 10_000

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,7 @@ const config: HardhatUserConfig = {
           viaIR: true,
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 10000,
             details: {
               yulDetails: {
                 optimizerSteps: "u",
