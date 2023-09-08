437a270cbf9b7b2a400dbdad861486473ac8aa65
Andrew Warner <warneracw@gmail.com>
2023-09-08 11:26:09 -0400

    update escrow implementations

--- contracts/escrow/ERC721EscrowBase.sol
+++ contracts/escrow/ERC721EscrowBase.sol
@@ -6,7 +6,12 @@ import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721H
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC721EscrowBase is Ownable, ERC721Holder {
-    constructor(address conduit, address collection) Ownable() ERC721Holder() {
-        IERC721(collection).setApprovalForAll(conduit, true);
+    address public immutable conduit;
+    address public immutable collection;
+    
+    constructor(address _conduit, address _collection) Ownable() ERC721Holder() {
+        IERC721(_collection).setApprovalForAll(_conduit, true);
+        conduit = _conduit;
+        collection = _collection;
     }
 }
