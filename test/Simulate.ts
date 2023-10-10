ab5e52422a9623b4a9a29c3608328b7ba11853cd
Andrew Warner <warneracw@gmail.com>
2023-10-10 18:26:07 -0400

    add amortizing test

--- test/Simulate.ts
+++ test/Simulate.ts
@@ -7,7 +7,7 @@ import {
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
 
-import { formatEther } from "ethers";
+import { formatEther, parseEther } from "ethers";
 
 import { getFixture } from './setup';
 import {
@@ -128,8 +128,9 @@ describe("Kettle", () => {
             totalAmount: loanAmount * BigInt(LOAN_COUNT),
             minAmount: loanAmount,
             maxAmount: loanAmount,
-            duration: DAY_SECONDS * 30,
-            rate: 1217,
+            // duration: DAY_SECONDS * 30,
+            duration: 2628000,
+            rate: 1000,
             expiration: blockTimestamp + DAY_SECONDS * 365,
             fees: [{
               rate: "21",
@@ -256,6 +257,187 @@ describe("Kettle", () => {
 
           // expect lender balance to equal total interest paid
           expect(await testErc20.balanceOf(lender)).to.equal((interestAmount) * BigInt(i + 1));
+        }
+
+        // repay loan
+        await testErc20.mint(borrower, interestAmount);
+        await testErc20.mint(borrower, feeAmount);
+
+        await kettle.connect(borrower).repay(lien, lienId);
+
+        console.log(
+          `Repay Loan\t|\t`, 
+          await testErc20.balanceOf(borrower).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          await testErc20.balanceOf(lender).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          await testErc20.balanceOf(feeRecipient).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          (BigInt(lien.startTime) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+          "\t|\t",
+          (BigInt(lien.startTime) + BigInt(lien.duration) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+          "\t|"
+        );
+      })
+
+      it('should refinance with the same loan offer (amortizing)', async () => {
+        const LOAN_COUNT = 12;
+
+        // expect initail balances
+        expect(await testErc20.balanceOf(lender)).to.equal(loanAmount);
+        expect(await testErc20.balanceOf(borrower)).to.equal(0);
+
+        // construct offer and signature
+        ({ offer, offerSignature, offerHash } = await prepareLoanOffer(
+          kettle,
+          lender,
+          {
+            collateralType: CollateralType.ERC721,
+            collateralIdentifier: tokenId1,
+            lender: lender,
+            collection: testErc721,
+            currency: testErc20,
+            totalAmount: loanAmount * BigInt(LOAN_COUNT),
+            minAmount: 0,
+            maxAmount: loanAmount,
+            // duration: DAY_SECONDS * 30,
+            duration: 2628000,
+            rate: 1000,
+            expiration: blockTimestamp + DAY_SECONDS * 365,
+            fees: [{
+              rate: "0",
+              recipient: await feeRecipient.getAddress()
+            }]
+        }));
+
+        // construct auth and signature
+        ({ auth, authSignature } = await prepareLoanOfferAuth(
+          kettle,
+          authSigner,
+          borrower,
+          await time.latest() + DAY_SECONDS * 365,
+          offer,
+          {
+            collateralType: CollateralType.ERC721,
+            tokenId: tokenId1,
+            collection: testErc721,
+            amount: 1
+          }
+        ));
+
+        // start loan
+        let txn = await kettle.connect(borrower).borrow(
+          offer,
+          auth,
+          offerSignature,
+          authSignature,
+          loanAmount,
+          tokenId1,
+          ADDRESS_ZERO,
+          []
+        );
+
+        // extract lien and lien id
+        ({ lien, lienId } = await txn.wait().then(
+          (receipt) => extractLien(receipt!, kettle)
+        ));
+
+        // initialize lien parameters
+        const initialTime = lien.startTime;
+
+        console.log("\nAction\t\t|\tBorrower|\tLender\t|\tFees\t|\tStart\t|\tDue\t|");
+        console.log("-------------------------------------------------------------------------------------------------");
+
+        console.log(
+          "Start Loan\t|\t", 
+          await testErc20.balanceOf(borrower).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          await testErc20.balanceOf(lender).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          await testErc20.balanceOf(feeRecipient).then(b => parseFloat(formatEther(b)).toFixed(2)),
+          "\t|\t",
+          (BigInt(lien.startTime) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+          "\t|\t",
+          (BigInt(lien.startTime) + BigInt(lien.duration) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+          "\t|"
+        );
+
+        // get repayment amount
+        repaymentAmount = await kettle.getRepaymentAmount(
+          lien.borrowAmount,
+          lien.rate,
+          lien.duration
+        );
+
+        // initialize fee amount
+        let feeAmount = loanAmount * BigInt(offer.fees[0].rate) / BigInt(10000);
+        
+        // initialize interest amount
+        let interestAmount = repaymentAmount - loanAmount;
+
+        // expect balances are updated
+        expect(await testErc20.balanceOf(lender)).to.equal(0);
+        expect(await testErc20.balanceOf(borrower)).to.equal(loanAmount - feeAmount);
+
+        // *****************************
+        // setup of cascading refinance
+        // *****************************
+        let lastRepayAmount = repaymentAmount;
+        for (var i=0; i<LOAN_COUNT-1; i++) {
+
+          await testErc20.mint(borrower, interestAmount);
+          await testErc20.mint(borrower, feeAmount);
+          await time.setNextBlockTimestamp(BigInt(await time.latest()) + BigInt(lien.duration) - BigInt(DAY_SECONDS * 10));
+
+          // expect initial borrower balance to equal the repayment amount
+          // expect(await testErc20.balanceOf(borrower)).to.equal(repaymentAmount);
+
+          const 
+          txn = await kettle.connect(borrower).refinance(
+            lien,
+            lienId,
+            // repaymentAmount - parseEther((1.646879439 * (i + 1)).toString()),
+            lastRepayAmount - parseEther("0.8791588723"),
+            offer,
+            auth,
+            offerSignature,
+            authSignature,
+            []
+          );
+
+          // extract lien and lien id
+          ({ lien, lienId } = await txn.wait().then(
+            (receipt) => extractLien(receipt!, kettle)
+          ));
+
+          lastRepayAmount = await kettle.getRepaymentAmount(
+            lien.borrowAmount,
+            lien.rate,
+            lien.duration
+          );
+
+          console.log(
+            `Refi Loan ${i + 1}\t|\t`, 
+            await testErc20.balanceOf(borrower).then(b => parseFloat(formatEther(b)).toFixed(2)),
+            "\t|\t",
+            await testErc20.balanceOf(lender).then(b => parseFloat(formatEther(b)).toFixed(2)),
+            "\t|\t",
+            await testErc20.balanceOf(feeRecipient).then(b => parseFloat(formatEther(b)).toFixed(2)),
+            "\t|\t",
+            (BigInt(lien.startTime) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+            "\t|\t",
+            (BigInt(lien.startTime) + BigInt(lien.duration) - BigInt(initialTime)) / BigInt(DAY_SECONDS),
+            "\t|"
+          );
+
+          // expect the difference in loan times to be the duration on early repayment
+          // expect(lien.startTime).to.equal(BigInt(lien.duration) * BigInt(i + 1 ) + BigInt(initialTime));
+
+          // // expect borrower balance to just be loan amount
+          // expect(await testErc20.balanceOf(borrower)).to.equal(loanAmount - feeAmount);
+
+          // // expect lender balance to equal total interest paid
+          // expect(await testErc20.balanceOf(lender)).to.equal((interestAmount) * BigInt(i + 1));
         }
 
         // repay loan
