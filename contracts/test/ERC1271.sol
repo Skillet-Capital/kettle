018a7e23650c6f80f3b04301dcaeb918bf568a81
Andrew Warner <warneracw@gmail.com>
2024-02-01 13:27:37 -0500

    mock erc1271 signer

---
+++ contracts/test/ERC1271.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ERC1271WalletMock is Ownable, IERC1271 {
+    constructor() Ownable() {}
+
+    function isValidSignature(bytes32 hash, bytes memory signature) public view returns (bytes4 magicValue) {
+        return ECDSA.recover(hash, signature) == owner() ? this.isValidSignature.selector : bytes4(0);
+    }
+
+    function approveCurrencyForOperator(address operator, address currency) public onlyOwner {
+        IERC20(currency).approve(operator, type(uint256).max);
+    }
+}
+
+contract ERC1271MaliciousMock is IERC1271 {
+    function isValidSignature(bytes32, bytes memory) public pure returns (bytes4) {
+        assembly {
+            mstore(0, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            return(0, 32)
+        }
+    }
+}
